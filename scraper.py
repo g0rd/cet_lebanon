ab6dd4164f92f895031b1b16fa1b943b4c16286b
g0rd <r.devashrayee@gmail.com>
2015-09-02 18:59:17 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -28,7 +28,8 @@ with Browser("phantomjs") as browser:
        # details = project.find_by_tag("tr td")[1]
         print name.value
         overlay = browser.find_by_css("#fancybox-wrap")
-        overlay.mouse_out().click()
+        overlay.mouse_out()
+        browser.click()
         #print details.get_text()
 
 # # Write out to the sqlite database using scraperwiki library
