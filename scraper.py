5f15348e3922bd78e426966e0534f26bef2651ad
g0rd <r.devashrayee@gmail.com>
2015-09-02 18:26:23 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -24,7 +24,9 @@ with Browser("phantomjs") as browser:
         tr.click()
         project = browser.find_by_css("#project_box table")
         name = project.first.find_by_tag("td tr")
-        print (name)
+        details = project.second.find_by_tag("td tr")
+        print name.get_text()
+        print details.get_text()
 
 # # Write out to the sqlite database using scraperwiki library
 
