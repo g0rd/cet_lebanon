1c441d85b7e63b21f4d26f5a8949839e59e04dcf
g0rd <r.devashrayee@gmail.com>
2015-09-02 21:33:15 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -31,7 +31,7 @@ with Browser("phantomjs") as browser:
         rows = project.find_by_tag("tr")
         name = rows[0].find_by_tag("td")
         print name.value
-        overlay = browser.find_by_id("#fancybox-close")
+        overlay = browser.find_by_id("fancybox-close")
         overlay.style.display = "none"
         time.sleep(0.5)
                 
