ce974ca916a1c947ec32eaaa0fe37ced37117b93
g0rd <r.devashrayee@gmail.com>
2015-09-02 21:39:39 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -34,7 +34,7 @@ with Browser("phantomjs") as browser:
         overlay = browser.find_by_id("fancybox-close")
         try:
             overlay.click()
-        except (httplib, urllib2):
+        except httplib.BadStatusLine:
             pass
         time.sleep(0.5)
                 
